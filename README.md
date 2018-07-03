c2b3ed6eb144ad68d18b784eb121f258918df0dd
alfianverter <38290702+alfianverter@users.noreply.github.com>
2018-07-03 09:05:28 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# 12
+# Lisa
+
+A discord bot with miscellaneous features
+
+## Introduction
+
+This bot is named after the plant Lisa from the Video game "Life is Strange".
+<http://dontnodentertainment.wikia.com/wiki/Lisa_the_Plant>
+
+It is a discord bot running on Node.js with Discord.js.
+
+To get started using Lisa, check `$help` for a list of commands.
+
+## Usage
+
+If you want the bot to join your server,
+go to <https://discordapp.com/oauth2/authorize?&client_id=269802184571486218&scope=bot> and confirm your server.
+
+If you want to modify the bot, you should start by cloning this repository & installing the npm modules.
+
+### Setup
+
+After you cloned the repository, running `npm install` from your CLI should install all dependencies.
+
+Then, add your bot-key to the environment variables as `DISCORD_KEY` (you could use another name as long as you update the index.js)
+
+After you've done that, running `node index.js` will start the bot.
+
+### Content
+
+Lisa is split into modules:
+
+*   core: Contains general commands like `help`, `about` or `invite`
+*   lisa: Lisa-minigame commands like `status`, `water` or `pet`
+*   fun: Miscellaneous commands for all kinds of stuff
+*   tag: Use and create YNA tags, based on the popular discord-bot 42's tag system. Check `$help tag` for more help.
+*   pokemon: pokemon related data lookups
+*   users: searches/matches users
+*   roles: role related commands
+*   data: general data commands
+
+### Optional dependencies:
+
+```shell
+npm install uws erlpack@discordapp/erlpack --no-save
+```
---
+++ test.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const Dingy = require("di-ngy");
+const commands = require("./lib/commands");
+const onInit = require("./lib/events/onInit");
+const onConnect = require("./lib/events/onConnect");
+const onMessage = require("./lib/events/onMessage");
+
+const config = {
+    token: process.env.DISCORD_TOKEN_TEST,
+    prefix: "$$",
+    dataPersisted: {
+        dir: "./data/",
+        files: [
+            "lisa",
+
+            "tag_storage",
+
+            "pokemon_abilities",
+            "pokemon_items",
+            "pokemon_moves",
+            "pokemon_pokedex",
+            "pokemon_typechart",
+            "pokemon_format"
+        ]
+    },
+    roles: [
+        {
+            name: "Admin",
+            power: 10,
+            assignable: false,
+            check: msg =>
+                [
+                    "128985967875850240" //Nobo
+                ].includes(msg.author.id)
+        },
+        {
+            name: "Trusted",
+            power: 10,
+            assignable: false,
+            check: msg =>
+                [
+                    "178470784984023040", //Niklas
+                    "236226432970391556", //Lilla
+                    "80403171238748160", //Fraw
+                    "78541183818674176" //Squas
+                ].includes(msg.author.id)
+        },
+        {
+            name: "User",
+            power: 1,
+            assignable: true,
+            check: () => true
+        }
+    ],
+    options: {
+        logLevel: "debug" //Level of log messages recommended to be either "debug" or "info", but can be any supported log-level
+    }
+};
+const events = {
+    onInit,
+    onConnect,
+    onMessage
+};
+
+const bot = new Dingy(config, commands, {}, events);
+
+bot.connect();
